7c59cd0ec857ba3820e6791ff8b1ddbf8a77869f
suuzuux <xxhiyonixx@gmail.com>
2026-08-20 14:46:25 +0900

    master001
    20260820
    프로젝트 시작

---
+++ weplanet/src/main/java/_megane/weplanet/WeplanetApplication.java
@@ -0,0 +1,13 @@
+package _megane.weplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeplanetApplication.class, args);
+	}
+
+}
