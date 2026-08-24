7581fd3f2f869bec30f1a6f6304eb6ecc02dbd16
suuzuux <xxhiyonixx@gmail.com>
2026-08-24 09:31:48 +0900

    260824정휘원
    application.properties 수정 ('6megane' > 'megane6')

---
+++ weplanet/src/test/java/_megane/weplanet/WeplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package _megane.weplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
